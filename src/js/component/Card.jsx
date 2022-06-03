fd33d69ea90547085b6c51e09d586b736021d01a
majg1987 <majg1987@hotmail.com>
2022-06-03 11:22:21 +0000

    logica de añadir y eliminar completada

---
+++ src/js/component/Card.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+const Card = ({ tareas, setTareas }) => {
+	const eliminar = (element) => {
+		let filter = tareas.filter((tarea) => {
+			return tarea !== element;
+		});
+		setTareas(filter);
+	};
+
+	return (
+		<>
+			<div className="card mt-3 mx-auto">
+				<div className="card-body border border-info">
+					<ul className="list-group-item border border-warning">
+						{tareas.length === 0 ? (
+							<p className="text-info">Añade Tareas a la Lista</p>
+						) : (
+							tareas.map((element, index) => {
+								return (
+									<li
+										key={index}
+										className="d-flex justify-content-between text-info p-2">
+										{element}
+										<span onClick={() => eliminar(element)}>
+											<i className="far fa-trash-alt"></i>
+										</span>
+									</li>
+								);
+							})
+						)}
+					</ul>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Card;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,45 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import Card from "./Card.jsx";
 
 //create your first component
 const Home = () => {
+	const [tarea, setTarea] = useState("");
+	const [tareas, setTareas] = useState([]);
+	const [alert, setAlert] = useState(false);
+
+	function añadir(e) {
+		if (e.key === "Enter") {
+			if (tarea === "") {
+				setAlert(true);
+			}
+			if (tarea.length > 0) {
+				setAlert(false);
+				tareas.push(tarea);
+				setTarea("");
+			}
+		}
+	}
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1 className="text-center text-primary">TodoList</h1>
+			<div className="input-group flex-nowrap">
+				<input
+					type="text"
+					className="form-control mx-auto border border-warning"
+					placeholder="Introduce tu Tarea"
+					aria-label="Username"
+					aria-describedby="addon-wrapping"
+					onChange={(e) => setTarea(e.target.value)}
+					onKeyDown={añadir}
+				/>
+			</div>
+			<Card tareas={tareas} setTareas={setTareas} />
+			<div
+				className={`${alert ? "alert alert-danger" : ""}`}
+				role="alert">
+				{`${alert ? "No puedes insertar una tarea vacia" : ""}`}
+			</div>
 		</div>
 	);
 };
